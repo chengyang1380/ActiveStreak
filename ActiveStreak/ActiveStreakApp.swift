153e05e4ffd330558e44d31a25040690d1ab5446
chengyang1380 <18714826+chengyang1380@users.noreply.github.com>
2025-02-18 22:49:01 +0800

    Added new Xcode project activeStreak.

---
+++ ActiveStreak/ActiveStreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActiveStreakApp.swift
+//  ActiveStreak
+//
+//  Created by ChengYangChen on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ActiveStreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ActiveStreakTests/ActiveStreakTests.swift
@@ -0,0 +1,17 @@
+//
+//  ActiveStreakTests.swift
+//  ActiveStreakTests
+//
+//  Created by ChengYangChen on 2/18/25.
+//
+
+import Testing
+@testable import ActiveStreak
+
+struct ActiveStreakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
